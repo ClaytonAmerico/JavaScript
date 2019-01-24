a0c9890a5cb95fa676fe2c665c0bac190eeca4e7
Clayton Américo de Oliveira <Clayton.Oliveira@gft.com>
2019-01-24 13:15:28 -0200

    Remoção concluida

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,8 +1,17 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-pacientes.forEach(function(paciente){
-	paciente.addEventListener("dblclick", function(){
-		console.log("Fui clicado com duplo click");
-		this.remove();
-	});
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", function(event){
+	//var alvoEvento = event.target;
+	//var paiDoAlvo = alvoEvento.parentNode; // TR = paciente = remover
+	//paiDoAlvo.remove();
+	
+	
+	
+	event.target.parentNode.classList.add("fadeOut");
+	setTimeout(function(){
+		event.target.parentNode.remove();
+	}, 500);
+	
 });
